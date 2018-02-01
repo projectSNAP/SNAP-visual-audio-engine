fe2a5d1d0a7e612ff4d184e822876c66a1346e4b
Dustin Fox <dustin.o.fox@gmail.com>
2018-01-31 19:15:26 -0800

    added post-build action to copy opencv_world340.dll to output directory

--- SNAP-visual-audio-engine/stdafx.h
+++ SNAP-visual-audio-engine/stdafx.h
@@ -8,12 +8,6 @@
 #include "targetver.h"
 
 // TODO: reference additional headers your program requires here
-
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-#include <windows.h>
-#include <conio.h>
-#include <stdio.h>
-#include <tchar.h>
 #include "al.h"
 #include "alc.h"
 #include "BlenderToDepthMapDLL.h"
@@ -22,4 +16,10 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 #include <fstream>
+// Windows specific includes
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
+#include <windows.h>
+#include <conio.h>
+#include <stdio.h>
+#include <tchar.h>
 #include <direct.h>
