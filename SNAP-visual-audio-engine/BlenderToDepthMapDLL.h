694d14788785f1ecc1f0d5f58d836198b1f05315
Dustin <dustin.o.fox@gmail.com>
2018-05-07 10:30:51 -0700

    removed BlenderToDepthMapDLL

--- SNAP-visual-audio-engine/BlenderToDepthMapDLL.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
- void* CreateDepthBufMapFile();
- int UnmapDepthBufFile(void* UnmapFileStruct);
- void* OpenDepthBufMapFileToRead(int x, int y);
- void* ReadDepthMapBufFile(void* InDataStruct);
- int CheckDMBFlag(void* InDataStruct);
